88070e8e46d24bb9a2ea9fea5837d64a45535388
Nicholas Pung <nicholaspung>
2019-05-07 15:11:11 -0700

    finished 1 stretch exercise for closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -21,7 +21,23 @@ sampleClosure('nickel');
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  function counting() {
+    count++;
+    return count;
+  }
+
+  return counting;
 };
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
