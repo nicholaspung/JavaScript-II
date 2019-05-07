8c504b607dcecd318720f9d7f263dca0e0a1b8d6
Nicholas Pung <nicholaspung>
2019-05-07 13:03:48 -0700

    WIP: finished callbacks exercises

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,5 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a 
+// solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -24,27 +25,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function firstItem(arr, cb) {
+  return cb(arr[0]);
+}
+
+firstItem(items, function(first) {
+  console.log(first);
+});
+
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+getLength(items, function(arr) {
+  console.log(arr.length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(last) {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
+
+sumNums(1,2, function(x,y) {
+  console.log(x+y);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
 }
+
+multiplyNums(5, 10, function(x,y) {
+  console.log(x*y);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
+
+contains('Notebook', items, function(item, list) {
+  console.log(list.includes(item));
+});
 
 /* STRETCH PROBLEM */
 
